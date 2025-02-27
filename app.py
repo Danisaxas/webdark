4752657544b72d575eea5ea5ca85a13657a66415
Danisaxas <danluismay@gmail.com>
2025-02-27 12:53:01 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect("https://darkabout.xyz/desarrollador")
 
 @app.route('/desarrollador')
 def desarrollador():
