ed8030bf2565e8d8bb0a5a6f42221be18c5e3e0e
Danisaxas <danluismay@gmail.com>
2025-02-27 12:50:50 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -2,32 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Rutas principales
-@app.route('/')
-@app.route('/portada')
-def portada():
-    return render_template('index.html')
-
-@app.route('/historia')
-def historia():
-    return render_template('historia.html')
-
-@app.route('/ciencia')
-def ciencia():
-    return render_template('ciencia.html')
-
-@app.route('/arte')
-def arte():
-    return render_template('arte.html')
-
 @app.route('/desarrollador')
 def desarrollador():
     return render_template('desarrollador.html')
-
-# Página de error 404
-@app.errorhandler(404)
-def pagina_no_encontrada(error):
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
