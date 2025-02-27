eda3ba4af93db62c7c090e5134811f1f5dcfd69c
Danisaxas <danluismay@gmail.com>
2025-02-27 13:28:26 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal que muestra la página 'desarrollador.html'
 @app.route('/')
 def index():
     return render_template('desarrollador.html')
 
-# Página de error 404
 @app.errorhandler(404)
 def pagina_no_encontrada(error):
     return render_template('404.html'), 404
